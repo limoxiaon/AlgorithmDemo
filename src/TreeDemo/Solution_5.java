bb65f334cb9bba4fb21ab67464e2ac73958d59ce
jun_moxiao <li_moxiao@126.com>
2020-07-07 17:07:42 +0800

    路径总和

--- src/TreeDemo/Solution_5.java
+++ src/TreeDemo/Solution_5.java
@@ -1,4 +1,24 @@
 package TreeDemo;
-
+//求二叉树的最大深度
 public class Solution_5 {
+
+    public int maxDepth(TreeNode root) {
+        if(root==null){
+            return 0;
+        }
+
+        int left_depth=maxDepth(root.left);
+        int right_depth=maxDepth(root.right);
+
+        return max(left_depth,right_depth)+1;
+    }
+
+    public int max(int left,int right){
+        if(left>right){
+            return left;
+        }else {
+            return right;
+        }
+    }
+
 }
